112821b54a64a3e92ba91da9099f941aefa0a252
Shengxiang Ji <shengxiang.ji123@gmail.com>
2022-02-11 23:25:00 -0500

    kill processes before launching gunicorn

--- start.sh
+++ start.sh
@@ -18,6 +18,10 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
+# Kill any process that's sitting on the ports now
+sudo kill -9 `sudo lsof -t -i:5000`
+sudo kill -9 `sudo lsof -t -i:5001`
+
 echo "> Starting the memcache app on port 5001"
 gunicorn --bind 0.0.0.0:5001 --workers=1 memcacheapp:webapp &> memcacheapp_log.txt &
 
