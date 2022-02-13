6e0a827d0f50329714bbebfd2734be4ade343bea
TernalWang <ternal.wang@mail.utoronto.ca>
2022-02-13 16:21:50 -0500

    add key get image structure

--- frontendapp/pages/key.py
+++ frontendapp/pages/key.py
@@ -6,3 +6,15 @@ from flask import json
 @webapp.route('/key',methods=['GET'])
 def key_form():
     return render_template("pages/key/key_form.html",title = "KEY")
+
+@webapp.route('/key',methods=['POST'])
+def key_save():
+    error_msg = None
+
+    key_input = request.form.get("key_input")
+    if key_input == "":
+        error_msg = "Missing key"
+        return render_template("pages/key/key_form.html",title = "KEY",error_msg = error_msg)
+
+    error_msg = "KEY SUCCESS"
+    return render_template("pages/key/key_form.html",title = "KEY",error_msg = error_msg)    
