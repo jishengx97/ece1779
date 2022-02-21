3ee9e7fee745d9eaf296872298e03fead437502e
Shengxiang Ji <shengxiang.ji123@gmail.com>
2022-02-21 04:36:16 +0000

    change the way that the plots are generated to (hopefully) improve performance

--- frontendapp/pages/show_stats.py
+++ frontendapp/pages/show_stats.py
@@ -7,6 +7,7 @@ import io
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.ticker import MultipleLocator
+import matplotlib.pyplot as plt
 import base64
 import math
 
@@ -37,56 +38,65 @@ def show_stats():
         miss_rate.append(item.miss_rate)
         hit_rate.append(item.hit_rate)
         stats_timestamp.append(item.stats_timestamp.strftime("%X"))
-    print (num_items)
-    print (total_size)
-    print (num_requests_served)
-    print (miss_rate)
-    print (hit_rate)
-    print (stats_timestamp)
+    # print (num_items)
+    # print (total_size)
+    # print (num_requests_served)
+    # print (miss_rate)
+    # print (hit_rate)
+    # print (stats_timestamp)
     
-    num_items_img = get_html_decoded_figure(
-        stats_timestamp, 
-        num_items, 
-        'Number of Items in Memcache', # title
-        'Time (Eastern)', # xlabel
-        'Items' # ylabel
-    )
+    # num_items_img = get_html_decoded_figure(
+    #     stats_timestamp, 
+    #     num_items, 
+    #     'Number of Items in Memcache', # title
+    #     'Time (Eastern)', # xlabel
+    #     'Items' # ylabel
+    # )
 
-    total_size_img = get_html_decoded_figure(
-        stats_timestamp, 
-        total_size, 
-        'Total Size of Items in Memcache', # title
-        'Time (Eastern)', # xlabel
-        'Byte(s)' # ylabel
-    )
+    # total_size_img = get_html_decoded_figure(
+    #     stats_timestamp, 
+    #     total_size, 
+    #     'Total Size of Items in Memcache', # title
+    #     'Time (Eastern)', # xlabel
+    #     'Byte(s)' # ylabel
+    # )
 
-    num_requests_served_img = get_html_decoded_figure(
-        stats_timestamp, 
-        num_requests_served, 
-        'Number of Requests Served by Memcache', # title
-        'Time (Eastern)', # xlabel
-        'Requests' # ylabel
-    )
+    # num_requests_served_img = get_html_decoded_figure(
+    #     stats_timestamp, 
+    #     num_requests_served, 
+    #     'Number of Requests Served by Memcache', # title
+    #     'Time (Eastern)', # xlabel
+    #     'Requests' # ylabel
+    # )
 
-    miss_rate_img = get_html_decoded_figure(
-        stats_timestamp, 
-        miss_rate, 
-        'Missrate', # title
-        'Time (Eastern)', # xlabel
-        'Percentages' # ylabel
-    )
+    # miss_rate_img = get_html_decoded_figure(
+    #     stats_timestamp, 
+    #     miss_rate, 
+    #     'Missrate', # title
+    #     'Time (Eastern)', # xlabel
+    #     'Percentages' # ylabel
+    # )
 
-    hit_rate_img = get_html_decoded_figure(
-        stats_timestamp, 
-        hit_rate, 
-        'Hitrate', # title
-        'Time (Eastern)', # xlabel
-        'Percentages' # ylabel
-    )
+    # hit_rate_img = get_html_decoded_figure(
+    #     stats_timestamp, 
+    #     hit_rate, 
+    #     'Hitrate', # title
+    #     'Time (Eastern)', # xlabel
+    #     'Percentages' # ylabel
+    # )
 
+    y_axes = [num_items, total_size, num_requests_served, miss_rate, hit_rate]
+    titles = ['Number of Items in Memcache','Total Size of Items in Memcache','Number of Requests Served by Memcache','Missrate','Hitrate']
+    xlabels = ['Time (Eastern)', 'Time (Eastern)', 'Time (Eastern)', 'Time (Eastern)', 'Time (Eastern)']
+    ylabels = ['Items',  'Byte(s)', 'Requests', 'Percentages', 'Percentages']
+
+    img_all_in_one = get_html_decoded_figure_all_in_one(stats_timestamp, y_axes, titles, xlabels, ylabels)
+
+    # return render_template("pages/show_stats/show_stats_form.html", title = "STATS", error_msg = None, 
+    #     num_items_img=num_items_img, total_size_img=total_size_img, num_requests_served_img=num_requests_served_img,
+    #     miss_rate_img=miss_rate_img, hit_rate_img=hit_rate_img)
     return render_template("pages/show_stats/show_stats_form.html", title = "STATS", error_msg = None, 
-        num_items_img=num_items_img, total_size_img=total_size_img, num_requests_served_img=num_requests_served_img,
-        miss_rate_img=miss_rate_img, hit_rate_img=hit_rate_img)
+        img=img_all_in_one)
 
 def get_html_decoded_figure(x_axis, y_axis, title, xlabel, ylabel):
     output = io.BytesIO()
@@ -109,6 +119,38 @@ def get_html_decoded_figure(x_axis, y_axis, title, xlabel, ylabel):
     #         label.set_visible(False)
 
     num_items_figure.savefig(output, format='png')
+    output.seek(0)
+    figdata_png = base64.b64encode(output.getvalue())
+    return figdata_png.decode('utf8')
+
+def get_html_decoded_figure_all_in_one(x_axis, y_axes, titles, xlabels, ylabels):
+    assert len(y_axes) == len(titles) and len(y_axes) == len(xlabels) and len(y_axes) == len(ylabels), "Length should be identical!"
+    output = io.BytesIO()
+
+    num_items_figure = plt.figure(figsize=(8, 6*len(y_axes)))
+    for i in range(len(y_axes)):
+        y_axis = y_axes[i]
+        title = titles[i]
+        xlabel = xlabels[i]
+        ylabel = ylabels[i]
+
+        axis = num_items_figure.add_subplot(len(y_axes), 1, i+1)
+        num_items_plot = axis.plot(x_axis, y_axis)
+        axis.set_ylim(bottom=0)
+        axis.set_title(title)
+        axis.set_xlabel(xlabel)
+        axis.set_ylabel(ylabel)
+
+        # don't need to show so many ticks, set max number of ticks to 6
+        n_bins = 6
+        every_nth = max(1, math.ceil(len(x_axis)/6))
+        axis.xaxis.set_major_locator(MultipleLocator(every_nth))
+        axis.xaxis.set_minor_locator(MultipleLocator(1))
+    # for n, label in enumerate(axis.xaxis.get_ticklabels()):
+    #     if n % every_nth != 0:
+    #         label.set_visible(False)
+
+    num_items_figure.savefig(output, format='png',bbox_inches='tight')
     output.seek(0)
     figdata_png = base64.b64encode(output.getvalue())
     return figdata_png.decode('utf8')
