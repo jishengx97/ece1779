46c8d00c1c76d193af9a3791a13c73feddf815fe
TernalWang <ternal.wang@mail.utoronto.ca>
2022-02-21 03:41:33 -0500

    upload finish

--- frontendapp/pages/upload.py
+++ frontendapp/pages/upload.py
@@ -2,7 +2,8 @@ import requests
 from flask import render_template, url_for, request
 from frontendapp import webapp
 from flask import json
-
+import os
+from common import models
 
 @webapp.route('/upload',methods=['GET'])
 def upload_form():
@@ -26,26 +27,38 @@ def upload_save():
         error_msg = "Missing file name"
         return render_template("pages/upload/upload_form.html", title = "UPLOAD", error_msg = error_msg)
 
-    local_session = webapp.db_session()
-    result = local_session.query(models.KeyAndFileLocation).filter(models.KeyAndFileLocation.key == key_input)
+    file_filename, file_extension = os.path.splitext(new_file.filename)
+    result = webapp.db_session.query(models.KeyAndFileLocation).filter(models.KeyAndFileLocation.key == key_input)
     if(result.count() == 1):
         # key exists, update file, send invalidate request
+        os.remove(result.first().file_location)
+        new_file.filename = str(result.first().id)+file_extension 
+        new_file.save(os.path.join("/home/ubuntu/ece1779/images", new_file.filename))
+        result.first().file_location = "/home/ubuntu/ece1779/images/"+new_file.filename
         r = requests.post("http://127.0.0.1:5001/invalidateKey", data={'key':key_input,})
+        if r.status_code == 200:
+            error_msg = "KEY ALREADY EXIST SUCCESS UPDATE THE IMAGE"
+        else:
+            error_msg = r.json()   
     else:
         # key doesn't exist, create new entry
         new_entry = models.KeyAndFileLocation(
             key = key_input,
-            file_location = "file_location"
+            file_location = "/home/ubuntu/ece1779/images"
         )
-        local_session.add(new_entry)
-        local_session.commit()
+        webapp.db_session.add(new_entry)
+        webapp.db_session.commit()
         # must refresh before accessing
-        local_session.refresh(new_entry)
-        print(new_entry.id)
-        # use new_entry.id here
-
-    if r.status_code == 200:
-        error_msg = "UPLOAD SUCCESS"
-    else:
-        error_msg = r.json()  
+        webapp.db_session.refresh(new_entry)
+        if not os.path.exists("/home/ubuntu/ece1779/images"):
+            os.makedirs("/home/ubuntu/ece1779/images")
+        new_file.filename = str(new_entry.id)+file_extension    
+        new_file.save(os.path.join("/home/ubuntu/ece1779/images", new_file.filename))
+        new_entry.file_location = "/home/ubuntu/ece1779/images/"+new_file.filename
+        webapp.db_session.commit()
+        r = requests.post("http://127.0.0.1:5001/invalidateKey", data={'key':key_input,})
+        if r.status_code == 200:
+            error_msg = "SUCCESS UPLOAD THE NEW IMAGE"
+        else:
+            error_msg = r.json()
     return render_template("pages/upload/upload_form.html", title = "UPLOAD", error_msg = error_msg)
