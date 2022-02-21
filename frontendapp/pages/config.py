e9d733423d3a08e4be73f5979f5c5b5b719595eb
TernalWang <ternal.wang@mail.utoronto.ca>
2022-02-20 22:55:40 -0500

    init value for config page

--- frontendapp/pages/config.py
+++ frontendapp/pages/config.py
@@ -1,6 +1,7 @@
 from flask import render_template, url_for, request
 from frontendapp import webapp
 from flask import json
+from common import models
 def is_float(element):
     try:
         float(element)
@@ -11,20 +12,31 @@ def is_float(element):
 
 @webapp.route('/config',methods=['GET'])
 def config_form():
-    return render_template("pages/config/config_form.html", title = "CONFIG", error_msg = None)
+    obj = webapp.db_session.query(models.MemcacheConfig).first()
+    init_policy = obj.replacement_policy
+    init_capacity = obj.capacity_in_mb
+    return render_template("pages/config/config_form.html", title = "CONFIG", policys = ["LRU", "RANDOM"], init_capacity = init_capacity, init_policy = init_policy, error_msg = None)
 
 @webapp.route('/config',methods=['POST'])
 def config_save():
     error_msg = None
 
+    obj = webapp.db_session.query(models.MemcacheConfig).first()
+    init_policy = obj.replacement_policy
+    init_capacity = obj.capacity_in_mb
+
     capacity_input = request.form.get("capacity_input")
+    policy_input = request.form.get('policy_input')
 
     if is_float(capacity_input) == False:
         error_msg = "Please type in a valid float"
-        return render_template("pages/config/config_form.html", title = "CONFIG", error_msg = error_msg)
+        return render_template("pages/config/config_form.html", title = "CONFIG", policys = ["LRU", "RANDOM"], init_capacity = init_capacity, init_policy = init_policy, error_msg = error_msg)
 
     capacity_input = float(capacity_input)
-
+    
+    if capacity_input == init_capacity and policy_input == init_capacity:
+        error_msg = "Error nothing changed"
+        return render_template("pages/config/config_form.html", title = "CONFIG", policys = ["LRU", "RANDOM"], init_capacity = init_capacity, init_policy = init_policy, error_msg = error_msg)
     # if capacity_input == "":
     #     error_msg = "Missing key"
     #     return render_template("pages/key/key_form.html",title = "KEY",error_msg = error_msg, img = None)
@@ -38,4 +50,4 @@ def config_save():
     #     error_msg = r.json()
     #     img = None
 
-    return render_template("pages/config/config_form.html", title = "CONFIG", error_msg = error_msg)
+    return render_template("pages/config/config_form.html", title = "CONFIG", policys = ["LRU", "RANDOM"], init_capacity = init_capacity, init_policy = init_policy, error_msg = error_msg)
