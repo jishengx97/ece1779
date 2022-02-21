4f4250ffc36b9f0d3f9499b31559b80a6fb24da3
Ubuntu <ubuntu@ip-172-31-80-8.ec2.internal>
2022-02-21 09:28:33 +0000

    finish list_key

--- frontendapp/pages/list_keys.py
+++ frontendapp/pages/list_keys.py
@@ -1,8 +1,11 @@
 from flask import render_template, url_for, request
 from frontendapp import webapp
 from flask import json
-
+from common import models
 
 @webapp.route('/list_keys',methods=['GET'])
 def list_keys_form():
-    return render_template("pages/list_keys/list_keys_form.html", title = "LIST_KEYS", error_msg = None, img = None)
+    local_session = webapp.db_session()
+    result = local_session.query(models.KeyAndFileLocation)
+
+    return render_template("pages/list_keys/list_keys_form.html", title = "LIST_KEYS", lists = result, error_msg = None)
