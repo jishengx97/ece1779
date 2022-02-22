8dd94de7863ab83b55b0092d250d704f5686f539
Zihao Chen <46636554+chenzi26@users.noreply.github.com>
2022-02-22 04:10:58 +0000

    finish auto test script

--- tests/autotests.py
+++ tests/autotests.py
@@ -4,8 +4,8 @@ IMAGE_DIR = 'images/'
 def test():
     # first upload three images
     image_png = open(IMAGE_DIR+'pngtest.png', 'rb')
-    image_jpeg = open(IMAGE_DIR+'jpegtest.png', 'rb')
-    image_gif = open(IMAGE_DIR+'figtest.png', 'rb')
+    image_jpeg = open(IMAGE_DIR+'jpegtest.jpeg', 'rb')
+    image_gif = open(IMAGE_DIR+'giftest.gif', 'rb')
 
     if not perform_upload_and_test(image_png, "pngkey"):
         print("png test failed!")
@@ -21,8 +21,9 @@ def test():
     
 
 def perform_upload_and_test(image, key):
-    files = dict("key" = key, "file" = image)
-    response = requests.post("127.0.0.1:5000/api/upload", data=files)
+    #files = dict("key" = key, "file" = image)
+    value = {"key":key}
+    response = requests.post("http://54.166.254.10:5000/api/upload", data=value,files={"file":image})
 
     try:
         response_data = response.json()
@@ -35,7 +36,7 @@ def perform_upload_and_test(image, key):
         return False
 
 def get_all_keys_and_test(keys):
-    response = requests.post("127.0.0.1:5000/api/list_keys")
+    response = requests.post("http://54.166.254.10:5000/api/list_keys")
     response_data = response.json()
 
     try:
@@ -51,3 +52,9 @@ def get_all_keys_and_test(keys):
             return False
     except:
         return False
+
+def main():
+    test()
+
+if __name__ == "__main__":
+    main()
