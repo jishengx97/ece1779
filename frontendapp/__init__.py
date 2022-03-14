f10b1a0691077ffaef41b755badb3eaab38ab3d3
jishengx97 <shengxiang.ji123@gmail.com>
2022-03-14 02:55:53 +0000

    move s3_bucket_name to .env file since they are unique within the region

--- frontendapp/__init__.py
+++ frontendapp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, _app_ctx_stack
 from sqlalchemy.orm import scoped_session
 from common import database
+from decouple import config
 
 webapp = Flask(__name__)
 webapp.url_map.strict_slashes = False
@@ -8,7 +9,7 @@ webapp.url_map.strict_slashes = False
 database.init_db()
 
 webapp.db_session = scoped_session(database.SessionLocal, scopefunc=_app_ctx_stack.__ident_func__)
-s3_bucket_name = 'ece17791'
+s3_bucket_name = config('AWS_STORAGE_BUCKET_NAME')
 
 # initialzes the database and populates them with default values if necessary
 from frontendapp import initialize_db
