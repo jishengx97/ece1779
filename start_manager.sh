6dbf56066f629e8e110fb6028ac55d8795f9dc27
Shengxiang Ji <shengxiang.ji123@gmail.com>
2022-03-23 22:31:51 -0400

    sleep longer

--- start_manager.sh
+++ start_manager.sh
@@ -30,6 +30,6 @@ echo "> Starting the manager app on port 5000"
 gunicorn --bind 0.0.0.0:5000 --timeout 0 --workers=1 --threads=3 --capture-output --log-level debug run_managerapp:webapp &> managerapp_log.txt &
 
 # Wait a bit to allow setup to properly finish
-sleep 5
+sleep 10
 
 python3 auto_scaler.py &> auto_scaler_log.txt &
